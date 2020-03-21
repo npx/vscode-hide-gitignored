831f29fb8d94c2c94543a8c1c4e6421af828caf0
npx <>
2020-03-21 14:14:21 +0100

    feat(settings-accessor): fix strict errors

--- src/settings-accessor.ts
+++ src/settings-accessor.ts
@@ -26,24 +26,24 @@ export class SettingsAccessor {
 
         // keep excluded files that arent excluded via gitignore
         const show = this._buildSettingsObject(patterns);
-        const newSettings = {};
+        const newSettings: any = {};
         for (const key in hidden) {
             if (key in show) {
                 continue;
             }
             newSettings[key] = hidden[key];
-        } 
+        }
 
         settings.update('files.exclude', newSettings);
     }
 
     private getWorkspaceValue(settings: WorkspaceConfiguration): any | undefined {
-        return settings.inspect('files.exclude').workspaceValue;
+        return settings.inspect('files.exclude')?.workspaceValue;
     }
 
     private _buildSettingsObject(patterns: Pattern[]): any {
-        const object = {};
-        patterns.forEach((pattern) => (object[pattern.glob] = pattern.hide));
+        const object: any = {};
+        patterns.forEach(pattern => (object[pattern.glob] = pattern.hide));
         return object;
     }
 }
